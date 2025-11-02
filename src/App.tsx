a5e18b4c6407f1b6210a06298078cfeb52f9d782
Amrit-2708 <rrajamrit.21@gmail.com>
2025-11-03 03:15:39 +0530

    removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// // fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-// // ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-
 import React, { useState, useCallback, useEffect, useMemo } from "react";
 import SearchBar from "./components/SearchBar";
 import RepoCard from "./components/RepoCard";
@@ -75,7 +72,6 @@ const App = () => {
   );
 
   return (
-    // <div className="max-w-4xl mx-auto p-4">
     <div className="max-w-5xl mx-auto px-4 py-8">
       <header className="mb-6">
         <h1 className="text-2xl font-bold text-white">GitHub Bookmarks</h1>
